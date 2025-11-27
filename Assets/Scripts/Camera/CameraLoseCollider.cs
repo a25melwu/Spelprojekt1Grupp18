bc2c3b2c961134dcfe071cf8773ba4e2b87607f4
stortrut <a25alvfe@student.his.se>
2025-11-27 14:40:36 +0100

    Added offset option for the camera lose collider

--- Assets/Scripts/Camera/CameraLoseCollider.cs
+++ Assets/Scripts/Camera/CameraLoseCollider.cs
@@ -2,13 +2,19 @@ using UnityEngine;
 
 public class CameraLoseCollider : MonoBehaviour
 {
+    [Header("Variables")]
+    [Tooltip("The amount the collider is under the camera edge")]
+    [SerializeField] private float yOffset = 3f;
+
+    [Header("References")]
     [SerializeField] private BoxCollider2D loseCollider;
+
 
     private void Start()
     {
         //Set the colliders position to be at the edge of the screen - this way it will be correct even if the player plays on a weird screen
         Camera cam = Camera.main;
-        transform.position = new Vector2(cam.transform.position.x, cam.transform.position.y - cam.orthographicSize);
+        transform.position = new Vector2(cam.transform.position.x, cam.transform.position.y - cam.orthographicSize - yOffset);
         float width = cam.orthographicSize * 2f * cam.aspect;
         loseCollider.size = new Vector2(width, loseCollider.size.y);
     }
