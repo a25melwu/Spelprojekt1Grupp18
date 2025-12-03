be0600b48348001cc03e7e1255c0d8f4d20aa189
a25melwu <a25melwu@student.his.se>
2025-12-03 13:00:27 +0100

    MainMenu added in Speltest1Level

---
+++ Assets/Scripts/UI/InstantiateUIDoublejump.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InstantiateUIDoublejump : MonoBehaviour
+{
+    [SerializeField] private GameObject imagePrefab;
+    [SerializeField] private Transform parentImageHolder;
+
+    private int instanceCount = 0;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        // if we haven't filled in a specific position, set parentImageHolder to this GameObjects transform
+        if (parentImageHolder == null)
+        {
+            parentImageHolder = transform;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public void AddUIDoubleJump(int value)
+    {
+        if (imagePrefab == null || value <= 0) return;
+
+        for (int i = 0; i < value; i++)
+        {
+            GameObject instance = Instantiate(imagePrefab, parentImageHolder);
+            instanceCount += value;
+        }
+        
+    }
+
+    public void ClearUIDoubleJump()
+    {
+        instanceCount = 0;
+    }
+
+    public int GetUIDoubleJump()
+    {
+        return instanceCount;
+    }
+}
