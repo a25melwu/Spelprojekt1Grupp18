c22cdac9f69e9fa9efe9461f64255685f001b6d7
a25melwu <a25melwu@student.his.se>
2026-01-05 11:14:21 +0100

    New script for the end dialogue
    freezes player and opens main menu when the end dialogue ends

---
+++ Assets/Scripts/UI/EndDialogue.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class EndDialogue : MonoBehaviour
+{
+    private PlayerInput playerInput;
+    private MainMenu mainMenu;
+    
+    //used together with e.g. UnityEventOnTrigger, UI-button-events to decide when a scene should be changed or the game should be closed
+    [Tooltip("Enter next scene or leave empty")]
+    public string scene;
+
+    void Awake()
+    {
+        playerInput = FindFirstObjectByType<PlayerInput>();
+        mainMenu = FindFirstObjectByType<MainMenu>();
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SwitchToUI()
+    {
+        if (playerInput != null)
+        {
+            playerInput.SwitchCurrentActionMap("UI");
+            Debug.Log($"End dialogue - switched to {playerInput.currentActionMap?.name}");
+        }
+    }
+
+    public void OpenMainMenu()
+    {
+        if (mainMenu != null)
+        {
+            mainMenu.OnToggleMenuButton();
+        }
+        
+    }
+    
+    public void ChangeScene()
+    {
+        string scene = this.scene.ToString();
+        if (string.IsNullOrEmpty(scene)) return;
+        
+        SceneManager.LoadSceneAsync(scene, LoadSceneMode.Single);
+        
+    }
+}
